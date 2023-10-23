7efd630e76626536f763ac316499057e4cb34609
GMaiaa <gabrielmaiaa22@gmail.com>
2023-10-23 20:27:46 -0300

    Delete done

--- src/main/java/com/example/quitandafrescor/controller/ProductController.java
+++ src/main/java/com/example/quitandafrescor/controller/ProductController.java
@@ -1,17 +1,24 @@
 package com.example.quitandafrescor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.quitandafrescor.product.ProductRequestDTO;
 import com.example.quitandafrescor.product.ProductResponseDTO;
+
 import com.example.quitandafrescor.product.Product;
 import com.example.quitandafrescor.product.ProductRepository;
 
@@ -24,7 +31,7 @@ public class ProductController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void saveProduct(@RequestBody ProductRequestDTO data){
+    public void saveProduct(@RequestBody ProductRequestDTO data) {
         Product productData = new Product(data);
 
         repository.save(productData);
@@ -33,9 +40,16 @@ public class ProductController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public List<ProductResponseDTO> getAll(){
+    public List<ProductResponseDTO> getAll() {
         List<ProductResponseDTO> productList = repository.findAll().stream().map(ProductResponseDTO::new).toList();
         return productList;
     }
-    
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        Optional<Product> optionalProduct = repository.findById(id);
+            repository.delete(optionalProduct.get());
+    }
+
 }
