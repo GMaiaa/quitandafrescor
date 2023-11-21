44f2b0030ce61805612b7a6e61e5615a3fe48974
yuritakamidev <yuri.takamisawa.ribeiro@gmail.com>
2023-11-21 00:04:15 -0300

    update

--- src/main/java/com/example/quitandafrescor/repository/ItemCartRepository.java
+++ src/main/java/com/example/quitandafrescor/repository/ItemCartRepository.java
@@ -1,11 +1,14 @@
 package com.example.quitandafrescor.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.quitandafrescor.model.ItemCart;
+import com.example.quitandafrescor.model.Product;
 
 @Repository
 public interface ItemCartRepository extends JpaRepository<ItemCart, Long> {
-
+    List<ItemCart> findAllByProduct(Product product);
 }
